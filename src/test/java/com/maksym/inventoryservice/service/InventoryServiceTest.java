a8ad9b83aff40b8f994f9b01ec80feeda1408199
Maksym Makhrevych <makhrevychmaxim@gmail.com>
2024-03-19 10:50:26 -0500

    Added Validation, added tests for the Controllers and Services

--- src/test/java/com/maksym/inventoryservice/service/InventoryServiceTest.java
+++ src/test/java/com/maksym/inventoryservice/service/InventoryServiceTest.java
@@ -4,16 +4,12 @@ import com.maksym.inventoryservice.dto.InventoryRequest;
 import com.maksym.inventoryservice.dto.InventoryResponse;
 import com.maksym.inventoryservice.model.Inventory;
 import com.maksym.inventoryservice.repository.InventoryRepository;
-import jakarta.ws.rs.NotFoundException;
+import com.maksym.inventoryservice.staticObject.StaticInventory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +22,8 @@ import static org.mockito.Mockito.when;
 @ExtendWith(SpringExtension.class)
 public class InventoryServiceTest {
 
+    private final String id1 = "1";
+    private final String id2 = "2";
     @Mock
     private InventoryRepository inventoryRepository;
 
@@ -35,7 +33,7 @@ public class InventoryServiceTest {
     @Test
     public void testExistsById() {
         // Given
-        List<String> ids = Arrays.asList("id1", "id2");
+        List<String> ids = Arrays.asList(id1, id2);
         when(inventoryRepository.existsAllByIdIn(ids)).thenReturn(true);
 
         // When
@@ -48,8 +46,8 @@ public class InventoryServiceTest {
     @Test
     public void testAdd() {
         // Given
-        InventoryRequest request = new InventoryRequest(); // Create a valid request
-        Inventory inventory = new Inventory(); // Create a valid inventory
+        InventoryRequest request = StaticInventory.inventoryRequest1(); // Create a valid request
+        Inventory inventory = StaticInventory.inventory1(); // Create a valid inventory
         when(inventoryRepository.save(any(Inventory.class))).thenReturn(inventory);
 
         // When
@@ -62,13 +60,11 @@ public class InventoryServiceTest {
 
     @Test
     public void testGet() {
-        // Given
-        String id = "id1";
-        Inventory inventory = new Inventory(); // Create a valid inventory
-        when(inventoryRepository.findById(id)).thenReturn(Optional.of(inventory));
+        Inventory inventory = StaticInventory.inventory1(); // Create a valid inventory
+        when(inventoryRepository.findById(id1)).thenReturn(Optional.of(inventory));
 
         // When
-        InventoryResponse response = inventoryService.get(id);
+        InventoryResponse response = inventoryService.get(id1);
 
         // Then
         assertNotNull(response);
@@ -78,7 +74,7 @@ public class InventoryServiceTest {
     @Test
     public void testGetAll() {
         // Given
-        List<Inventory> inventoryList = Arrays.asList(new Inventory(), new Inventory()); // Create a list of inventories
+        List<Inventory> inventoryList = Arrays.asList(StaticInventory.inventory1(), StaticInventory.inventory2()); // Create a list of inventories
         when(inventoryRepository.findAll()).thenReturn(inventoryList);
 
         // When
@@ -92,14 +88,13 @@ public class InventoryServiceTest {
     @Test
     public void testUpdate() {
         // Given
-        String id = "id1";
         InventoryRequest request = new InventoryRequest(); // Create a valid request
         Inventory inventory = new Inventory(); // Create a valid inventory
-        inventory.setId(id);
+        inventory.setId(id1);
         when(inventoryRepository.save(any(Inventory.class))).thenReturn(inventory);
 
         // When
-        InventoryResponse response = inventoryService.update(id, request);
+        InventoryResponse response = inventoryService.update(id1, request);
 
         // Then
         assertNotNull(response);
@@ -108,11 +103,8 @@ public class InventoryServiceTest {
 
     @Test
     public void testDelete() {
-        // Given
-        String id = "id1";
-
         // When
-        boolean result = inventoryService.delete(id);
+        boolean result = inventoryService.delete(id1);
 
         // Then
         assertTrue(result);
